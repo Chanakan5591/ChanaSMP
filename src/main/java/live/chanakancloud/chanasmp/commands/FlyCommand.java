130d86e79c9d1d366caa90c47a719926d423c4fe
Chanakan Mungtin <chanakan4591@gmail.com>
2020-11-15 07:43:23 +0700

    Fix FlyCommand import

--- src/main/java/live/chanakancloud/chanasmp/commands/FlyCommand.java
+++ src/main/java/live/chanakancloud/chanasmp/commands/FlyCommand.java
@@ -1,5 +1,10 @@
 package live.chanakancloud.chanasmp.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 public class FlyCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
